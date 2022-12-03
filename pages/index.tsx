9cc1221fadf95d2fabb118a9899fb0606200255c
jackie <bobbinnej@gmail.com>
2022-12-03 10:39:44 +0300

    complete first part of hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,22 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 
 export default function Home() {
   return (
-    <div >
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory
+    overflow-scroll z-0' >
       <Head>
         <title>Jackies Portfolio</title>      
       </Head>
 
-      {/**Header section */}
         <Header/>
       {/**Hero section */}
+      <section id='hero' className='snap-center'>
+        <Hero/>
+      
+      </section>
 
       {/**About section */}
 
